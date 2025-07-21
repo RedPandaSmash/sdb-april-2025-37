4559f5f1d2c3a848e8f717a8b70d45a27b72f80b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-21 17:53:45 -0500

    state and ternary

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import UserProfile from './components/UserProfile'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
 export default function App() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(true)
+
   return (
-    <div>hello from app!</div>
+    <div>{isLoggedIn ? <UserProfile /> : <LoginForm />}</div>
   )
 }
